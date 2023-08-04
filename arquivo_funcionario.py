686a08fb693e302deb3643d89b1198fd9c49d531
Wesley <wsyalmeida@gmail.com>
2023-08-04 13:37:04 -0300

    Adicionado alguns objetivos ao zé - se ele olha o git status ou log ele irá me falar

--- arquivo_funcionario.py
+++ arquivo_funcionario.py
@@ -1,2 +1,23 @@
 Programe usando o mesmo metodologia de raciocinio do "arquivo_produto.py"
 Observo que arquivo em python são chamados módulo e pastas/diretório de pacotes
+
+
+dados do funcionário
+
+nome,
+cpf - se quiser pode validar se o usuario tem o cpf válido,
+idade
+cargo - depois iremos criar outro arquivo com cargo, porque cargo tem atributos
+
+
+Menu deve Conter 
+
+adicionar
+alterar,
+remover_especifico,
+remover_tudo
+lista 
+sair
+
+Depois iremos e integrar
+por exemplo funcionário que cadastrou produto, funcionario que removeu, funcionario do caixa que realizou a venda
